b3a4ff36c48390162304f83f5f694d4f9c5929a4
Aamarsh <aamarshthakur723@gmail.com>
2026-02-23 14:34:19 +0530

    Added exp5 React lazy loading project

---
+++ exp5/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <div>
+      <h1>Welcome to Profile</h1>
+      <p>This is where users can view their information.</p>
+    </div>
+  );
+}
+
+export default Profile;
